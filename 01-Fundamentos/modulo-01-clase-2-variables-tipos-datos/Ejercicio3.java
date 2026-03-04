db574e61dc857260c882e6b4d154f2e99aff78e8
Gerson Jara <46004475+Gersonjaragarcia@users.noreply.github.comQ>
2026-03-04 13:38:04 -0500

    feat: → Desarrollar el ejercicio 3

--- 01-Fundamentos/modulo-01-clase-2-variables-tipos-datos/Ejercicio3.java
+++ 01-Fundamentos/modulo-01-clase-2-variables-tipos-datos/Ejercicio3.java
@@ -1,3 +1,14 @@
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        int edad = 40;
+
+        int meses = edad * 12;
+        int dias = edad * 365;
+        int horas = edad * 25;
+
+        System.out.println("MI edad en años es: " + edad + "años");
+        System.out.print("Mi edad en meses es: " + meses + "meses");
+        System.out.println("Mi edad en dias es de: " + dias + "dias");
+        System.out.println("Mi edad horas es de: " + horas  + "Horas");
+    }
 }
